8c0c70310d7611db89d465c6c15dc528ad8602c1
Matthew <mw@matthewis.me>
2016-01-28 14:57:38 -0500

    exercises

--- classwork/lesson3_functions/functions.js
+++ classwork/lesson3_functions/functions.js
@@ -1,3 +1,4 @@
+/*
 // Functions are procedures that take arguments and return values.
 // Think about them like a sheet of paper with instructions on it.
 // define a function
@@ -46,9 +47,114 @@ function gratuity(argBillAmount) {
 function totalWithGrat(argAmount) {
 	tip = gratuity(argAmount);
 	totalBillWithTip = tip + argAmount;
+	totalBillWithTip = totalBillWithTip.toFixed(2);
 	return totalBillWithTip;
 }
 
 // console.log("gratuity for tonight = " + gratuity(billAmount));
-console.log("your total including gratuity is: " + totalWithGrat(130));
+console.log("your total including gratuity is: $" + totalWithGrat(132));
+
+// Like other objects, functions can be assigned to variables.
+// It's often handy to declare a function on the fly without a name.
+var calculator = {
+    add: function(a,b) {
+        return a + b;
+    }
+}
+
+var adding = calculator.add(12,33) // 5
+console.log("adding = " + adding);
+
+// array of array, object, function
+var arrayOfBartender = [
+	['anonymous', 'array'],
+	{ name: 'anonymous object' },
+	// anonymous function
+	function(){return 'anonymous function!'}
+]
+
+console.log(arrayOfBartender[0][1]) // array
+console.log(arrayOfBartender[1].name) // anonymous object
+console.log(arrayOfBartender[2]()) // anonymous function!
+*/
+
+// Exercise 2 (RPS Revisited)
+// Exercise 2 (RPS Revisited)
+// Exercise 2 (RPS Revisited)
+var hands = ['rock', 'paper', 'scissors'];
+var hand1;
+var hand2;
+var winner;
+var isDraw = false;
+
+var player1 = {
+	name: 'Russell',
+	getHand: function() {
+		return hands[parseInt(Math.random()*10)%3];
+	}
+}
+
+var player2 = {
+	name: 'Wilson',
+	getHand: function() {
+		return hands[parseInt(Math.random()*10)%3];
+	}
+}
+
+// function getHand() {
+// 	return hands[parseInt(Math.random()*10)%3];
+// }
+
+function playRound(argPlayerObj1, argPlayerObj2) {
+	hand1 = argPlayerObj1.getHand();
+	hand2 = argPlayerObj2.getHand();
+
+	if ( hand1 === "rock" && hand2 === "scissors" || hand1 === "paper" && hand2 === "rock" || hand1 === "scissors" && hand2 === "paper" ) {
+		winner = player1.name;
+	} else if ( hand1 === "rock" && hand2 === "paper" || hand1 === "paper" && hand2 === "scissors" || hand1 === "scissors" && hand2 === "rock" ) {
+		winner = player2.name;
+	} else {
+		isDraw = true;
+	}
+	
+	if (!isDraw) {
+		console.log("winner = " + winner + " Round: " + player1.name + "'s hand = " + hand1 + " " + player2.name + "'s hand = " + hand2);		
+	} else {
+		console.log("it's a tie");
+	}
+
+}
+
+function playGame(playerOne, playerTwo, playUntil) {
+	// figure it out
+	for (var i = 0; i < playUntil; i++) {
+		// play round
+		playRound(playerOne, playerTwo);
+	}
+
+}
+
+playGame(player1, player2, 5);
+// console.log(playRound(player1, player2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
