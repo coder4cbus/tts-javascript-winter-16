598d0118835588d0e237797991f3b44a4ed0b8b8
Matthew <mw@matthewis.me>
2016-01-28 12:11:16 -0500

    google

---
+++ classwork/lesson3_functions/functions.js
@@ -0,0 +1,9 @@
+// Functions are procedures that take arguments and return values.
+// Think about them like a sheet of paper with instructions on it.
+// define a function
+function doSomething(paramSomething) {
+	console.log(paramSomething);
+}
+
+// Execute a function by calling it's name with () and passing in arguments.
+doSomething("Helloooo!");
