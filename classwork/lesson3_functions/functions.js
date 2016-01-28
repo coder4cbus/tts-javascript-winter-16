99a4f015683dabc37b94d330b5e4f03861f9d989
Matthew <mw@matthewis.me>
2016-01-28 12:33:10 -0500

    exercise 1

--- classwork/lesson3_functions/functions.js
+++ classwork/lesson3_functions/functions.js
@@ -7,3 +7,48 @@ function doSomething(paramSomething) {
 
 // Execute a function by calling it's name with () and passing in arguments.
 doSomething("Helloooo!");
+
+// Functions can 'return' a value. A function evaluates to its return value when run.
+// All functions take any number of arguments, regardless of their declared signature.
+function doAddition(number1, number2) {
+	// explicitly 'return'
+	return number1 + number2;
+}
+
+var sum = doAddition(12, 14);
+console.log(sum);
+
+function moreAddition () {
+	var sum = 0;
+	for (var i = 0; i < arguments.length; i++) {
+		sum += arguments[i];
+	}
+	return sum;
+}
+
+var add = moreAddition(1,2,3,4,5,6,7);
+console.log(add);
+
+// Exercise 1 (function basics)
+// Exercise 1 (function basics)
+// Exercise 1 (function basics)
+var billAmount = 100;
+var tipAmount;
+var tip;
+var totalBillWithTip;
+
+function gratuity(argBillAmount) {
+	// do something
+	tipAmount = argBillAmount * 0.2;
+	return tipAmount;
+}
+
+function totalWithGrat(argAmount) {
+	tip = gratuity(argAmount);
+	totalBillWithTip = tip + argAmount;
+	return totalBillWithTip;
+}
+
+// console.log("gratuity for tonight = " + gratuity(billAmount));
+console.log("your total including gratuity is: " + totalWithGrat(130));
+
