150f5788ed05548ff5fb17b667c55f9e447de563
Matthew <mw@matthewis.me>
2016-02-02 12:31:41 -0500

    slideshow

--- classwork/lesson4_this/this.js
+++ classwork/lesson4_this/this.js
@@ -3,25 +3,40 @@ var slideshow = {
 	currentPhotoIndex: 0,
 	nextPhoto: function() {
 		if (this.currentPhotoIndex < this.photoList.length-1)  {
-			this.currentPhotoIndex += 1;
+			this.currentPhotoIndex++;
 			this.photoList[this.currentPhotoIndex];
 			console.log(this.photoList[this.currentPhotoIndex]);
 		} else {
 			console.log("End of slideshow!");
 		}
-	}
+	},
 	prevPhoto: function() {
-		if (this.currentPhotoIndex < this.photoList.length-1)  {
-			this.currentPhotoIndex -= 1;
+		if (this.currentPhotoIndex >= 1)  {
+			this.currentPhotoIndex--;
 			this.photoList[this.currentPhotoIndex];
 			console.log(this.photoList[this.currentPhotoIndex]);
 		} else {
-			console.log("End of slideshow!");
+			console.log("Begin slideshow!");
 		}
+	},
+	getCurrentPhoto: function() {
+		return console.log("Current photo is " + (this.photoList[this.currentPhotoIndex]));
 	}
 }
 
 slideshow.nextPhoto();
-
+slideshow.nextPhoto();
+slideshow.nextPhoto();
+slideshow.nextPhoto();
+slideshow.nextPhoto();
+slideshow.getCurrentPhoto();
+slideshow.getCurrentPhoto();
+slideshow.getCurrentPhoto();
+slideshow.prevPhoto();
+slideshow.prevPhoto();
+slideshow.prevPhoto();
+slideshow.prevPhoto();
+slideshow.prevPhoto();
+slideshow.getCurrentPhoto();
 
 
